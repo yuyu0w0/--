1a3dc97cf92409859208360d0e66cf12b72fb46d
ZHU Zhiyuan <144930938+yuyu0w0@users.noreply.github.com>
2024-03-24 21:14:18 +0800

    Add files via upload
    Signed-off-by: ZHU Zhiyuan <144930938+yuyu0w0@users.noreply.github.com>

---
+++ 算法1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+int MaxSubSequenceSum(const int A[], int N)
+{
+    int ThisSum, MaxSum, i, j, k;
+    MaxSum = 0;
+    for(i=0;i< N ;i++)
+    {
+        ThisSum = 0;
+        for(j=i;j< N ;j++)
+        {
+            for(k=i;k<= j ;k++)
+            {
+                ThisSum += A[k];
+                if(ThisSum > MaxSum)
+                    MaxSum = ThisSum;
+            }
+        }
+    }
+    return MaxSum;
+}
+int main()
+{
+    clock_t start,end;
+    double time;
+    int n,i,result;
+    printf("输入数据数");
+    scanf("%d",&n);
+    int a[n];
+    for(i = 0;i < n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    start = clock();
+    result=MaxSubSequenceSum(a,n);
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("最大子序列和为 %d \n",result);
+    printf("用时为 %f秒 \n",time);
+}
